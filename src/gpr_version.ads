594d93cd0e103169b47570dc125d9286333a7b5a
Olivier Ramonat <ramonat@adacore.com>
2012-11-07 10:35:19 +0000

    Bump version to 2.0.1
    Part of LB07-012
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/branches/global/gnat-7.1.1/gprbuild@197582 936e1b1b-40f2-da11-902a-00137254ae57

--- src/gpr_version.ads
+++ src/gpr_version.ads
@@ -24,7 +24,7 @@
 
 package GPR_Version is
 
-   Gpr_Version : constant String := "2.0.0w";
+   Gpr_Version : constant String := "2.0.1";
    --  Static string identifying this version
 
    function Gpr_Version_String return String;
