b97dd863ded4d46784df2d8f59ff8f21db9f6c84
Vincent Celier <celier@adacore.com>
2015-12-26 17:42:53 +0000

    Index of attribute Global_Compilation_Switches is always case-insensitive
    as it is a language name.
    Fix OC17-040
    
    git-svn-id: svn+ssh://svn.eu.adacore.com/Dev/trunk/gprbuild@245014
                936e1b1b-40f2-da11-902a-00137254ae57
    (cherry picked from commit c60d258b86de2a8084890ca26b2dcf034997d626)
    
    Change-Id: If48883f89c1613a3f73472a45ea5525646370698

--- gpr/src/gpr-attr.adb
+++ gpr/src/gpr-attr.adb
@@ -2,7 +2,7 @@
 --                                                                          --
 --                           GPR PROJECT MANAGER                            --
 --                                                                          --
---          Copyright (C) 2001-2015, Free Software Foundation, Inc.         --
+--          Copyright (C) 2001-2016, Free Software Foundation, Inc.         --
 --                                                                          --
 -- This library is free software;  you can redistribute it and/or modify it --
 -- under terms of the  GNU General Public License  as published by the Free --
@@ -240,7 +240,7 @@ package body GPR.Attr is
    "Pbuilder#" &
    "Ladefault_switches#" &
    "LcOswitches#" &
-   "Lcglobal_compilation_switches#" &
+   "Laglobal_compilation_switches#" &
    "Scexecutable#" &
    "SVexecutable_suffix#" &
    "SVglobal_configuration_pragmas#" &
