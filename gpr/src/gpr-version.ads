4f37dfbaf81f1f1a1d7234c0946b29933d0ef1c5
Vasiliy Fofanov <fofanov@adacore.com>
2017-10-06 13:03:20 +0200

    Set version to 18.0. QA03-029
    Change-Id: I3d1d3459cfa5340953405aceeb032958ef71f865

--- gpr/src/gpr-version.ads
+++ gpr/src/gpr-version.ads
@@ -28,7 +28,7 @@
 
 package GPR.Version is
 
-   Gpr_Version : constant String := "18.0w";
+   Gpr_Version : constant String := "18.0";
    --  Static string identifying this version
 
    Date : constant String := "19940713";
